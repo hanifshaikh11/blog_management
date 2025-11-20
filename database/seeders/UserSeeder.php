aab8c33c52a614ba788ccf68115ce0fbb450b7e9
Hanif Shaikh <hanifs@varenyamaritime.com>
2025-11-20 12:07:32 +0530

    New Data Added

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,30 +13,63 @@ class UserSeeder extends Seeder
     public function run(): void
     {
 
-        \DB::table('users')->truncate();
+        // \DB::table('users')->truncate();
 
         \App\Models\User::insert([
+            // [
+            //     'name' => 'Demo User',
+            //     'email' => 'demo@test.com',
+            //     'password' => Hash::make('123456'),
+            //     'created_at' => Carbon::now(),
+            //     'updated_at' => Carbon::now(),
+            // ],
+            // [
+            //     'name' => 'Another User',
+            //     'email' => 'abc@test.com',
+            //     'password' => Hash::make('123456'),
+            //     'created_at' => Carbon::now(),
+            //     'updated_at' => Carbon::now(),
+            // ],
+            // [
+            //     'name' => 'Hanif Shaikh',
+            //     'email' => 'hanif_blog@gmail.com',
+            //     'password' => Hash::make('123456'),
+            //     'created_at' => Carbon::now(),
+            //     'updated_at' => Carbon::now(),
+            // ],
+
             [
-                'name' => 'Demo User',
-                'email' => 'demo@test.com',
+                'name' => 'Tengo Charli',
+                'email' => 'charli@test.com',
                 'password' => Hash::make('123456'),
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now(),
             ],
+
             [
-                'name' => 'Another User',
-                'email' => 'abc@test.com',
+                'name' => 'Morgan',
+                'email' => 'morgan@test.com',
                 'password' => Hash::make('123456'),
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now(),
             ],
+
             [
-                'name' => 'Hanif Shaikh',
-                'email' => 'hanif_blog@gmail.com',
+                'name' => 'MS Patel',
+                'email' => 'patel@test.com',
                 'password' => Hash::make('123456'),
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now(),
-            ]
+            ],
+
+
+            [
+                'name' => 'Maria Go',
+                'email' => 'maria@test.com',
+                'password' => Hash::make('123456'),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
         ]);
     }
 }
